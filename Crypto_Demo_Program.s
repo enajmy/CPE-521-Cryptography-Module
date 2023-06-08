9f3b280a7ae51231d12ec0596043fdb35a393500
wkeitz <92413262+wkeitz@users.noreply.github.com>
2023-06-07 23:04:13 -0700

    Update Crypto_Demo_Program.s

--- Crypto_Demo_Program.s
+++ Crypto_Demo_Program.s
@@ -1,22 +1,26 @@
+.equ LEDS, 0x110C0000
+
+.global main 
+.type main, @function
+
 .data
 sseg: .byte 0x03, 0x9F, 0x25, 0x0D, 0x99, 0x49, 0x41, 0x1F, 0x01, 0x09
 
 .text
-li x7, 1000
-li x8, 100
-li x9, 10
-li x11, 0x110C0000  # SEGS
-li x12, 0x11000000 # SWITCHES
-li x13, 1
-li x14, 2
-li x21, 0x8000 # mask the left switch
-li x20, 0x4000 # mask the second left most switch
-li x19, 0x2000 # mask the third left most switch
-li x28, 0
-li x30, 0xFFF #mask 12 switches
-
-
-sw x13, 0(x11)
+main: 
+init:   li x7, 1000
+        li x8, 100
+        li x9, 10
+        li x11, 0x110C0000  # SEGS
+        li x12, 0x11000000 # SWITCHES
+        li x13, 1
+        li x14, 2
+        li x21, 0x8000 # mask the left switch
+        li x20, 0x4000 # mask the second left most switch
+        li x19, 0x2000 # mask the third left most switch
+        li x28, 0
+        li x30, 0xFFF #mask 12 switches
+        sw x13, 0(x11)
 
 loop1:
 # check if left switch is pushed, if so read the switches
@@ -36,14 +40,14 @@ beqz x23, loop2
 and x25, x22, x30
 mv x27, x25
 # encrypt the numbers (add)
-add x25, x25, x24
+encrypt x25, x25, x24
 mv x26, x25 #save encrypted value before converting to bcd
 call bcd
 sw x28, 0(x11)
 
 loop3:
 lhu x22, 0(x12)
-and x23, x19, x22
+decrypt x23, x19, x22
 beqz x23, loop3
 
 # decrypt the numbers (subtract)
@@ -52,7 +56,6 @@ call bcd
 sw x28, 0(x11)
 
 ret
-
 
 bcd: 	mv x28, x0
 thou:	blt x25, x7, hund
